9a6688747a4b42794adcfad13a3fb58982adf5f6
tanish0701 <tanish.singh@hatch.com>
2022-01-28 17:58:42 +0530

    change

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { stat } from 'fs';
+import { AccountsService } from '../accounts.service';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  // providers:[LoggingService]
+})
+export class NewAccountComponent {
+  /**
+   *
+   */
+  constructor(private loggingService:LoggingService,
+    private accountsService:AccountsService) 
+    {
+      this.accountsService.statusUpdated.subscribe(
+       (status:string) => alert('New Status: '+status)
+      );
+    }
+  
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountsService.addAccount(accountName,accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
+    // console.log('A server status changed, new status: ' + accountStatus);
+  }
+}
